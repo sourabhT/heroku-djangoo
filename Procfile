2d3c969d3d9581c401ebd42a04498fa619b5d03a
sourabhT <sourabh.turkar@gmail.com>
2021-03-28 18:23:37 +0530

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn crm1.wsgi
+web: gunicorn herokuract.wsgi
--- crm1/settings.py
+++ crm1/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'cz=&%f*9(d*zo$_55p=(p)(eki#p$pb^0159-)8k^6$9c3l&_b'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['python-djangoa-app.herokuapp.com']
 
 
 # Application definition
@@ -147,6 +147,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
